e99670a57ea17510d6edffce3689df9bc679d7c4
Jack HK <jackhughesking@gmail.com>
2024-07-01 00:40:14 +0100

    Added Destruction

--- src/main.cpp
+++ src/main.cpp
@@ -27,5 +27,8 @@ int main(int argc, char* args[])
 		//Hack to get window to stay up!
 		SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
 	}
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
 	return 0;
 }
